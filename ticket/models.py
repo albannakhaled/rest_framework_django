6a1ad4a435f6b51cb4df1996e3c034cfa1aa7837
albannakhaled <albannakhalil90@gmail.com>
2023-06-02 00:03:40 +0300

    auto take token for user (ticket/models)

--- ticket/models.py
+++ ticket/models.py
@@ -1,6 +1,8 @@
 from django.db import models
-
-
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
+from django.conf import settings
 
 # Create your models here.
 
@@ -22,3 +24,9 @@ class Guest(models.Model):
 class Reservation(models.Model):
     guest = models.ForeignKey(Guest, on_delete=models.CASCADE , related_name='reservation')
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE , related_name='reservation')
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
